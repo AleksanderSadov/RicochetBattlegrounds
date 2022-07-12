8257583f26308a8738aba43d3240ec4c70656f15
Aleksander Sadov <20453629+AleksanderSadov@users.noreply.github.com>
2022-07-12 18:23:18 +0700

    Refactor Trigger_LoadScene

---
+++ Assets/Code/Scripts/Game/Misc/TriggerLoadScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Unity.Ricochet.Game
+{
+    public class TriggerLoadScene : MonoBehaviour
+    {
+        public string sceneName;
+
+        private void OnTriggerEnter(Collider collision)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                SceneManager.LoadScene(sceneName);
+            }
+        }
+    }
+}
