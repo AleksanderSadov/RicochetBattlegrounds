48288bb00e40dfdece84e6377c7eee242d6372e4
Aleksander Sadov <20453629+AleksanderSadov@users.noreply.github.com>
2022-07-14 09:58:50 +0700

    Configure draw ricochet line max deflects limit

--- Assets/Code/Scripts/Gameplay/ProjectileRicochetLine.cs
+++ Assets/Code/Scripts/Gameplay/ProjectileRicochetLine.cs
@@ -4,34 +4,50 @@ namespace Unity.Ricochet.Gameplay
 {
     public class ProjectileRicochetLine : MonoBehaviour
     {
-        [SerializeField] GameObject gunPivot;
+        [SerializeField] private GameObject gunPivot;
+        [SerializeField] private int maxDeflectsCalculations = 1;
+        [SerializeField] private int currentDeflectsCount = 0;
 
         private void Update()
         {
-            Ray a = new Ray(gunPivot.transform.position, gunPivot.transform.forward);
-            Ray b;
-            RaycastHit hit;
+            Ray originRay = new Ray(gunPivot.transform.position, gunPivot.transform.forward);
+            Ray deflectedRay;
+            RaycastHit deflectHit;
 
-            if (Deflect(a, out b, out hit))
+            bool isDeflected;
+            currentDeflectsCount = 0;
+            do
             {
-                Debug.DrawLine(a.origin, hit.point);
-                Debug.DrawLine(b.origin, b.origin + 3 * b.direction);
+                isDeflected = IsDeflected(originRay, out deflectedRay, out deflectHit);
+
+                if (isDeflected)
+                {
+                    currentDeflectsCount++;
+                    Debug.DrawLine(originRay.origin, deflectHit.point);
+                    Debug.DrawLine(deflectedRay.origin, deflectedRay.origin + 3 * deflectedRay.direction);
+                    originRay = deflectedRay;
+                }
+                else
+                {
+                    Debug.DrawLine(originRay.origin, originRay.origin * 10);
+                }
             }
+            while (isDeflected && currentDeflectsCount < maxDeflectsCalculations);
         }
 
-        private bool Deflect(Ray ray, out Ray deflected, out RaycastHit hit)
+        private bool IsDeflected(Ray originRay, out Ray deflectedRay, out RaycastHit deflectHit)
         {
 
-            if (Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(originRay, out deflectHit))
             {
-                Vector3 normal = hit.normal;
-                Vector3 deflect = Vector3.Reflect(ray.direction, normal);
+                Vector3 normal = deflectHit.normal;
+                Vector3 deflect = Vector3.Reflect(originRay.direction, normal);
 
-                deflected = new Ray(hit.point, deflect);
+                deflectedRay = new Ray(deflectHit.point, deflect);
                 return true;
             }
 
-            deflected = new Ray(Vector3.zero, Vector3.zero);
+            deflectedRay = new Ray(Vector3.zero, Vector3.zero);
             return false;
         }
     }
