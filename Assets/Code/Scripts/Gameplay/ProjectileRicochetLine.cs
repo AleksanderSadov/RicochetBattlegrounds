09157c340036f7f895a2363e52d71c60b47adcd6
Aleksander Sadov <20453629+AleksanderSadov@users.noreply.github.com>
2022-07-14 11:27:31 +0700

    Draw ricochet line in game

--- Assets/Code/Scripts/Gameplay/ProjectileRicochetLine.cs
+++ Assets/Code/Scripts/Gameplay/ProjectileRicochetLine.cs
@@ -5,8 +5,16 @@ namespace Unity.Ricochet.Gameplay
     public class ProjectileRicochetLine : MonoBehaviour
     {
         [SerializeField] private GameObject gunPivot;
+        [SerializeField] private Material ricochetLineMaterial;
         [SerializeField] private int maxDeflectsCalculations = 1;
         [SerializeField] private int currentDeflectsCount = 0;
+
+        private LineRenderer lineRenderer;
+
+        private void Start()
+        {
+            InitLineRenderer();
+        }
 
         private void Update()
         {
@@ -16,6 +24,7 @@ namespace Unity.Ricochet.Gameplay
 
             bool isDeflected;
             currentDeflectsCount = 0;
+            lineRenderer.positionCount = 0;
             do
             {
                 isDeflected = IsDeflected(originRay, out deflectedRay, out deflectHit);
@@ -23,13 +32,18 @@ namespace Unity.Ricochet.Gameplay
                 if (isDeflected)
                 {
                     currentDeflectsCount++;
-                    Debug.DrawLine(originRay.origin, deflectHit.point);
-                    Debug.DrawLine(deflectedRay.origin, deflectedRay.origin + 3 * deflectedRay.direction);
+                    SetNextDrawLinePositions(originRay.origin, deflectedRay.origin);
+
+                    if (currentDeflectsCount == maxDeflectsCalculations)
+                    {
+                        SetNextDrawLinePositions(deflectedRay.origin + deflectedRay.direction * 3);
+                    }
+
                     originRay = deflectedRay;
                 }
                 else
                 {
-                    Debug.DrawLine(originRay.origin, originRay.origin * 10);
+                    SetNextDrawLinePositions(originRay.origin, originRay.origin + originRay.direction * 30);
                 }
             }
             while (isDeflected && currentDeflectsCount < maxDeflectsCalculations);
@@ -49,6 +63,35 @@ namespace Unity.Ricochet.Gameplay
 
             deflectedRay = new Ray(Vector3.zero, Vector3.zero);
             return false;
+        }
+
+        private void InitLineRenderer()
+        {
+            GameObject ricochetLine = new GameObject();
+            ricochetLine.transform.parent = gameObject.transform;
+            ricochetLine.transform.position = gunPivot.transform.position;
+            ricochetLine.name = "RicochetLine";
+            lineRenderer = ricochetLine.AddComponent<LineRenderer>();
+            lineRenderer.startWidth = 0.1f;
+            lineRenderer.endWidth = 0.1f;
+            lineRenderer.material = ricochetLineMaterial;
+        }
+
+        private void SetNextDrawLinePositions(params Vector3[] positions)
+        {
+            foreach(Vector3 position in positions)
+            {
+                int lastPositionIndex = lineRenderer.positionCount - 1;
+                if (lastPositionIndex > - 1
+                    && lineRenderer.GetPosition(lastPositionIndex).Equals(position))
+                {
+                    continue;
+                }
+
+                int nextPositionIndex = lineRenderer.positionCount;
+                lineRenderer.positionCount = nextPositionIndex + 1;
+                lineRenderer.SetPosition(nextPositionIndex, position);
+            }
         }
     }
 }
