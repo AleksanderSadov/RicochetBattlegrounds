a6f6805deeec13ac95a8b4ac64446b9c5defe295
Aleksander Sadov <20453629+AleksanderSadov@users.noreply.github.com>
2022-07-15 12:43:31 +0700

    Add random spawn at each new round

---
+++ Assets/Code/Scripts/Game/Managers/SpawnManager.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Unity.Ricochet.Game
+{
+    public class SpawnManager : MonoBehaviour
+    {
+        [Tooltip("Spawn radius in units from the spawner location")]
+        public int spawnRadius = 30;
+
+        [Tooltip("Spawner default location")]
+        public GameObject spawnDefaultPosition;
+
+        private ActorsManager actorsManager;
+
+        private void Start()
+        {
+            actorsManager = FindObjectOfType<ActorsManager>();
+            RandomSpawn();
+        }
+
+        private void RandomSpawn()
+        {
+            foreach (Actor actor in actorsManager.actors)
+            {
+                MoveToRandomSpawnPosition(actor.gameObject);
+            }
+        }
+
+        private void MoveToRandomSpawnPosition(GameObject entity)
+        {
+            Vector3 randomSpawnPosition;
+            if (FindRandomSpawnPosition(spawnDefaultPosition.transform.position, spawnRadius, out randomSpawnPosition))
+            {
+                entity.GetComponent<NavMeshAgent>().Warp(randomSpawnPosition);
+            }
+        }
+
+        private bool FindRandomSpawnPosition(Vector3 center, float range, out Vector3 result)
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                Vector3 randomPoint = center + new Vector3(
+                    Random.Range(-range, range),
+                    1.39f,
+                    Random.Range(-range, range)
+                );
+                NavMeshHit hit;
+                float maxHeight = 1.39f;
+                float maxDistance = maxHeight;
+                int walkableAreaMask = 1 << NavMesh.GetAreaFromName("Walkable");
+                if (NavMesh.SamplePosition(randomPoint, out hit, maxDistance, walkableAreaMask))
+                {
+                    result = hit.position;
+                    return true;
+                }
+            }
+            result = Vector3.zero;
+            return false;
+        }
+    }
+}
